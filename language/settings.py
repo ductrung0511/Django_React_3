acac7a624e34281621b7dde8d06657c97134c63f
ductrung0511 <ductrung.tran0511@gmail.com>
2024-03-06 20:24:39 +0700

    setting changed

--- language/settings.py
+++ language/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8wvulsxctwbxzzlvzpb*e8$@8qf+s4#*ll3e-j%(*h&)4^4vl_"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 from datetime import timedelta
 SIMPLE_JWT = {
